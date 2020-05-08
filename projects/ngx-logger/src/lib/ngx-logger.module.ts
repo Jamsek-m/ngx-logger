758a3441b720656d03d21c26398a93bac43c1694
Miha Jamsek <miha_jamsek@windowslive.com>
2020-05-08 14:21:37 +0200

    linted code

---
+++ projects/ngx-logger/src/lib/ngx-logger.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { NgxLoggerComponent } from "./ngx-logger.component";
+
+
+@NgModule({
+    declarations: [NgxLoggerComponent],
+    imports: [],
+    exports: [NgxLoggerComponent]
+})
+export class NgxLoggerModule {
+}
